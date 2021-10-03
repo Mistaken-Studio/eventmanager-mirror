a57428971a53f61c67090bccec4784dd119e4a20
Xname <xnameserver@o2.pl>
2021-10-03 15:41:05 +0200

    new event

---
+++ EventManager/Events/OOoX.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="OOoX.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using Mistaken.EventManager.EventCreator;
+
+namespace Mistaken.EventManager.Events
+{
+    internal class OOoX : IEMEventClass
+    {
+        public override string Id => "ooox";
+
+        public override string Description => "One Out of X";
+
+        public override string Name => "OOoX";
+
+        public override Dictionary<string, string> Translations => new Dictionary<string, string>()
+        {
+            // { "", "" }
+        };
+
+        public override void OnIni()
+        {
+        }
+
+        public override void OnDeIni()
+        {
+        }
+    }
+}
